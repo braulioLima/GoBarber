10a121492cbbaeece911d19f808f0765451b2528
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-05-11 17:30:01 -0300

    chore: add toast animation

--- frontend/src/components/ToastContainer/index.tsx
+++ frontend/src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import Toast from '../Toast';
 
@@ -11,11 +12,22 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: '0' },
+      enter: { right: '0%', opacity: '1' },
+      leave: { right: '-120%', opacity: '0' },
+    },
+  );
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} toastMessage={message} />
-      ))}
+      {messagesWithTransitions.map(
+        ({ item: message, key: messageID, props }) => (
+          <Toast key={messageID} style={props} toastMessage={message} />
+        ),
+      )}
     </Container>
   );
 };
