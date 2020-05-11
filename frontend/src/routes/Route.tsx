3fe60d2d3cbb0bc0ca9433a5ab405700a0371950
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-05-11 19:57:41 -0300

    feat: private routes

---
+++ frontend/src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Redirect,
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/AuthContext';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+// import { Container } from './styles';
+
+const Route: React.FC<RouteProps> = ({
+  component: Component,
+  isPrivate = false,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : 'dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
