668483cbcac107a3169db5e946f753f4b232ff68
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-05-15 12:14:00 -0300

    feat(mobile): private routes

--- mobile/src/hooks/AuthContext.tsx
+++ mobile/src/hooks/AuthContext.tsx
@@ -20,6 +20,7 @@ interface AuthState {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -33,6 +34,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
@@ -47,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
           user: JSON.parse(user[1]),
         });
       }
+
+      setLoading(false);
     }
 
     fetchData();
@@ -75,7 +79,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
