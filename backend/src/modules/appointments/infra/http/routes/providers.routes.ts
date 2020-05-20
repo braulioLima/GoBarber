0d44df1f528144264b38ac186ff38977c3069784
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-05-20 17:24:52 -0300

    chore(backend): routes/controller from list day availability

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -3,12 +3,26 @@ import { Router } from 'express';
 import ensureAuthenticatedMiddleware from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilyController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilyController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticatedMiddleware);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default providersRouter;
