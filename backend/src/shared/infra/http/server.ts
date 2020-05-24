e030c0fb2df832adfd608bef7eec5a16e97c10e1
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-05-24 17:09:05 -0300

    chore:prevent DDOS attack

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -2,12 +2,13 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
-import 'express-async-errors';
 
-import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -16,6 +17,7 @@ import '@shared/container';
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
