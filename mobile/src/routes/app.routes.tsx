ff73cd47ee401b7c015d65243beb018b6f6f2df5
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-06-10 20:24:24 -0300

    chore(mobile): add appointments routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
 
 const Auth = createStackNavigator();
 
@@ -12,7 +15,13 @@ const AppRoutes: React.FC = () => (
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
+    {/* Appointments routes */}
     <Auth.Screen name="Dashboard" component={Dashboard} />
+    <Auth.Screen name="CreateAppointment" component={CreateAppointment} />
+    <Auth.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    {/* User routes */}
+    <Auth.Screen name="Profile" component={Profile} />
   </Auth.Navigator>
 );
 
