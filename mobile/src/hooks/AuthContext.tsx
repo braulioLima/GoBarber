8629eba16fd1b8a9972be8a3ac1786c437b227d3
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-06-12 02:32:29 -0300

    chore(mobile):add update user hook

--- mobile/src/hooks/AuthContext.tsx
+++ mobile/src/hooks/AuthContext.tsx
@@ -18,6 +18,7 @@ interface AuthContextData {
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: UserDTO): Promise<void>;
 }
 
 interface SignInCredentials {
@@ -88,8 +89,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    async (user: UserDTO) => {
+      await AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [data.token, setData],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, loading, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
